e2250d56ebb904067ae097f10b904f73853daf1d
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2023-09-13 11:26:46 -0700

    debugged the code errors.

--- js/script.js
+++ js/script.js
@@ -14,11 +14,11 @@ function guessNumber(name, num) {
   }
 
   // We've got a number between 1 and 25, let's do this.
-  showguess.innerText = num;
+  showGuess.innerText = num;
 
   if (num === randomNumber) {
     response.innerText = `Hey, ${name}, you totally guessed the number! Must be your lucky day.`;
-  } elseif (num < randomNumber) {
+  } else if (num < randomNumber) {
     response.innerText = `Hey, ${name}, your guess was too low. The number was ${randomNumber}.`;
   } else if (num > randomNumber) {
     response.innerText = `Hey, ${name}, your guess was too high. The number was ${randomNumber}.`;
@@ -27,4 +27,4 @@ function guessNumber(name, num) {
   }
 }
 
-guessNumber(Octavia, num);
+guessNumber("Octavia", num);
